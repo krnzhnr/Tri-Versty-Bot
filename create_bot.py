a55db6813a27ba63b8707a280c917546655d5e4f
krnzhnr <alexey.polubenskiy@gmail.com>
2022-05-04 14:24:57 +0300

    new privetstvie

--- create_bot.py
+++ create_bot.py
@@ -3,5 +3,5 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot('5248251249:AAEjdP_4Ul0UxWDHPOr-kyAEWN9nkm7LPpY')
+bot = Bot('5328605145:AAEW-pJCnYLnFW_SbQWyB0NYbiOoEq5cE9k')
 dp = Dispatcher(bot, storage=storage)
